2f6c1743c97f6d4a49c7382f943b8661dbab6f6a
Natalia Kovrizhko <kovrizhko.n.i@gmail.com>
2022-09-29 17:02:01 +0300

    clear not used code

--- src/document-processor.js
+++ src/document-processor.js
@@ -21,32 +21,10 @@ function documentLoaded(){
         changeNameButton(nameBtn.value);
         buttonSettingsChanged(button);
     })
-
-    document.getElementById(HTMLConfig.ElementIds.loadStyleButton).addEventListener('click', () => {
-        let style = `
-            HTML:
-            <input id="button" type="button" value="${buttonSettings.text}">
-            
-            CSS:
-            #button {
-                color: ${buttonSettings.textColor};
-                font-family: ${buttonSettings.textFont};
-                font-size: ${buttonSettings.textSize};
-                font-weight: ${buttonSettings.textWeight};
-                background: ${buttonSettings.backgroundButton};
-            }
-        `
-        styles.innerText = style;
-    })
 }
 
 function buttonSettingsChanged(button){
     button.value = buttonSettings.text;
-    button.style.color = buttonSettings.textColor;
-    button.style.fontFamily = buttonSettings.textFont;
-    button.style.fontSize = `${buttonSettings.textSize}px`;
-    button.style.fontWeight = buttonSettings.textWeight;
-    button.style.background = buttonSettings.backgroundButton;
 }
 
 
