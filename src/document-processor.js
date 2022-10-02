61ef38871b2fadef9274c280271e78074e952942
Natalia Kovrizhko <kovrizhko.n.i@gmail.com>
2022-10-02 14:55:25 +0300

    Add ESlint

--- src/document-processor.js
+++ src/document-processor.js
@@ -1,30 +1,25 @@
-document.addEventListener("DOMContentLoaded", documentLoaded);
+document.addEventListener('DOMContentLoaded', documentLoaded);
 
 const HTMLConfig = {
-    ElementIds: {
-        CustomButton: "custom-button",
-        CustomButtonText: "custom-button-text",
-    }
+  ElementIds: {
+    CustomButton: 'custom-button',
+    CustomButtonText: 'custom-button-text',
+  },
+};
+
+function documentLoaded() {
+  const customButton = document.getElementById(HTMLConfig.ElementIds.CustomButton);
+  setupButtonTextInput(customButton);
 }
 
-function documentLoaded(){
-    const customButton = document.getElementById(HTMLConfig.ElementIds.CustomButton);
-    setupButtonTextInput(customButton)
+function setupButtonTextInput(button) {
+  const buttonTextInput = document.getElementById(HTMLConfig.ElementIds.CustomButtonText);
+  buttonTextInput.addEventListener('input', () => {
+    changeButtonText(buttonTextInput.value);
+    buttonSettingsChanged(button);
+  });
 }
 
-function setupButtonTextInput(button){
-    const buttonTextInput = document.getElementById(HTMLConfig.ElementIds.CustomButtonText);
-    buttonTextInput.addEventListener("input", () => {
-        hangeButtonText(buttonText.value);
-        buttonSettingsChanged(button);
-    })
+function buttonSettingsChanged(button) {
+  button.value = buttonSettings.text;
 }
-
-function buttonSettingsChanged(button){
-    button.value = buttonSettings.text;
-}
-
-
-
-
-
