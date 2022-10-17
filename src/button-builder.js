b41cb6c34e36ee9a7641b234a5b8b596c07ca41f
Natalia Kovrizhko <kovrizhko.n.i@gmail.com>
2022-10-17 17:45:25 +0300

    refactoring code and new functional

--- src/button-builder.js
+++ src/button-builder.js
@@ -1,49 +1,49 @@
-// eslint-disable-next-line import/no-mutable-exports
-export let buttonValue = 'My Button';
-
-export const buttonSettings = {
-  fontSize: {
-    cssName: 'fontSize',
-    value: '16',
-    additionalValue: 'px',
-  },
-  buttonBackground: {
-    cssName: 'background',
-    value: 'orange',
-    additionalValue: '',
-  },
-  textColor: {
-    cssName: 'color',
-    value: 'pink',
-    additionalValue: '',
-  },
-  fontFamily: {
-    cssName: 'fontFamily',
-    value: 'Arial',
-    additionalValue: '',
-  },
-};
+import buttonSettings from '../data/button-settings-data.js';
 
 export function changeButtonText(text) {
-  buttonValue = text;
+  buttonSettings.buttonValue = text;
 }
 
 export function changeButtonFontSize(size) {
-  buttonSettings.fontSize.value = size;
+  buttonSettings.style.fontSize.value = size;
 }
 
-export function changeMeasurement(measurement) {
-  buttonSettings.fontSize.additionalValue = measurement;
+export function changeFontSizeMeasurement(measurement) {
+  buttonSettings.style.fontSize.additionalValue = measurement;
 }
 
 export function changeButtonBackground(color) {
-  buttonSettings.buttonBackground.value = color;
+  buttonSettings.style.buttonBackground.value = color;
 }
 
 export function changeButtonTextColor(color) {
-  buttonSettings.textColor.value = color;
+  buttonSettings.style.textColor.value = color;
 }
 
 export function changeButtonFont(fontFamily) {
-  buttonSettings.fontFamily.value = fontFamily;
+  buttonSettings.style.fontFamily.value = fontFamily;
+}
+
+export function changeButtonPaddingSize(padding) {
+  buttonSettings.style.padding.value = padding;
+}
+
+export function changePaddingSizeMeasurement(measurement) {
+  buttonSettings.style.padding.additionalValue = measurement;
+}
+
+export function changeButtonBorderSize(padding) {
+  buttonSettings.style.borderSize.value = padding;
+}
+
+export function changeButtonBorderSizeMeasurement(measurement) {
+  buttonSettings.style.borderSize.additionalValue = measurement;
+}
+
+export function changeButtonBorderColor(color) {
+  buttonSettings.style.borderColor.value = color;
+}
+
+export function changeButtonBorderType(type) {
+  buttonSettings.style.borderType.value = type;
 }
--- src/document-processor.js
+++ src/document-processor.js
@@ -1,87 +1,97 @@
 import * as buttonBuilder from './button-builder.js';
-import { buttonSettings } from './button-builder.js';
+import buttonSettings from '../data/button-settings-data.js';
 
 document.addEventListener('DOMContentLoaded', documentLoaded);
 
 const HTMLConfig = {
   ElementIds: {
     CustomButton: 'custom-button',
-    CustomButtonText: 'custom-button-text',
-    CustomButtonFontSize: 'custom-button-font-size',
-    CustomButtonFontSizeMeasurement: 'custom-button-font-size-measurement',
-    CustomButtonBackground: 'custom-button-background',
     WindowForDisplayCssSettings: 'styles',
     ButtonForDisplayCssSettings: 'loadStyle',
-    CustomButtonTextColor: 'custom-text-color',
-    CustomButtonFont: 'custom-button-font',
   },
+  buttonInputSettings: [
+    {
+      elementId: 'custom-button-text',
+      handler: buttonBuilder.changeButtonText,
+    },
+    {
+      elementId: 'custom-button-font-size',
+      handler: buttonBuilder.changeButtonFontSize,
+    },
+    {
+      elementId: 'custom-button-font-size-measurement',
+      handler: buttonBuilder.changeFontSizeMeasurement,
+    },
+    {
+      elementId: 'custom-button-background',
+      handler: buttonBuilder.changeButtonBackground,
+    },
+    {
+      elementId: 'custom-text-color',
+      handler: buttonBuilder.changeButtonTextColor,
+    },
+    {
+      elementId: 'custom-button-font',
+      handler: buttonBuilder.changeButtonFont,
+    },
+    {
+      elementId: 'custom-button-padding-size',
+      handler: buttonBuilder.changeButtonPaddingSize,
+    },
+    {
+      elementId: 'custom-button-padding-measurement',
+      handler: buttonBuilder.changePaddingSizeMeasurement,
+    },
+    {
+      elementId: 'custom-button-border-size',
+      handler: buttonBuilder.changeButtonBorderSize,
+    },
+    {
+      elementId: 'custom-button-border-size-measurement',
+      handler: buttonBuilder.changeButtonBorderSizeMeasurement,
+    },
+    {
+      elementId: 'custom-button-border-color',
+      handler: buttonBuilder.changeButtonBorderColor,
+    },
+    {
+      elementId: 'custom-button-border-type',
+      handler: buttonBuilder.changeButtonBorderType,
+    },
+  ],
 };
 
 function documentLoaded() {
   const customButton = document.getElementById(HTMLConfig.ElementIds.CustomButton);
   buttonSettingsChanged(customButton);
-  setupButtonTextInput(customButton);
-  setupButtonFontSize(customButton);
-  setupButtonBackground(customButton);
-  setupButtonTextColor(customButton);
-  setupButtonFont(customButton);
   displayCssSettingsOnClick();
-}
 
-function setupButtonTextColor(button) {
-  const buttonTextColor = document.getElementById(HTMLConfig.ElementIds.CustomButtonTextColor);
-  buttonTextColor.addEventListener('input', () => {
-    buttonBuilder.changeButtonTextColor(buttonTextColor.value);
-    buttonSettingsChanged(button);
-  });
-}
-
-function setupButtonFont(button) {
-  const buttonFont = document.getElementById(HTMLConfig.ElementIds.CustomButtonFont);
-  buttonFont.addEventListener('input', () => {
-    buttonBuilder.changeButtonFont(buttonFont.value);
-    buttonSettingsChanged(button);
+  HTMLConfig.buttonInputSettings.forEach((inputSettings) => {
+    const inputElement = document.getElementById(inputSettings.elementId);
+    const eventNameToListen = getEventListenerNameByInputType(inputSettings.inputType ?? 'text');
+    inputElement.addEventListener(eventNameToListen, () => {
+      inputSettings.handler(inputElement.value);
+      buttonSettingsChanged(customButton);
+    });
   });
 }
 
-function setupButtonTextInput(button) {
-  const buttonTextInput = document.getElementById(HTMLConfig.ElementIds.CustomButtonText);
-  buttonTextInput.addEventListener('input', () => {
-    buttonBuilder.changeButtonText(buttonTextInput.value);
-    buttonSettingsChanged(button);
-  });
-}
-
-function setupButtonFontSize(button) {
-  const buttonFontSize = document.getElementById(HTMLConfig.ElementIds.CustomButtonFontSize);
-  const buttonFontSizeMeasurement = document.getElementById(HTMLConfig.ElementIds.CustomButtonFontSizeMeasurement);
-  const settingsButtonFontSize = () => {
-    buttonBuilder.changeButtonFontSize(buttonFontSize.value);
-    buttonBuilder.changeMeasurement(buttonFontSizeMeasurement.value);
-    buttonSettingsChanged(button);
-  };
-  buttonFontSize.addEventListener('input', () => {
-    settingsButtonFontSize();
-  });
-  buttonFontSizeMeasurement.addEventListener('change', () => {
-    settingsButtonFontSize();
-  });
-}
-
-function setupButtonBackground(button) {
-  const buttonBackground = document.getElementById(HTMLConfig.ElementIds.CustomButtonBackground);
-  buttonBackground.addEventListener('input', () => {
-    buttonBuilder.changeButtonBackground(buttonBackground.value);
-    buttonSettingsChanged(button);
-  });
+function getEventListenerNameByInputType(inputType = 'text') {
+  switch (inputType) {
+    case 'text':
+      return 'input';
+    default:
+      console.error('Not supported input field type');
+      return undefined;
+  }
 }
 
 function buttonSettingsChanged(button) {
-  button.value = buttonBuilder.buttonValue;
-  Object.keys(buttonSettings)
+  button.value = buttonSettings.buttonValue;
+  Object.keys(buttonSettings.style)
     .forEach((settingKey) => {
-      const cssElement = buttonBuilder.buttonSettings[settingKey].cssName;
-      button.style[cssElement] = `${buttonBuilder.buttonSettings[settingKey].value}${buttonBuilder.buttonSettings[settingKey].additionalValue}`;
+      const cssElement = buttonSettings.style[settingKey].cssName;
+      button.style[cssElement] = `${buttonSettings.style[settingKey].value}${buttonSettings.style[settingKey].additionalValue}`;
     });
 }
 
@@ -93,12 +103,12 @@ function displayCssSettings() {
   const windowForDisplayCssSettings = getElementToDisplayCssSettings();
   windowForDisplayCssSettings.innerHTML = '';
   let stringCssSettings = '';
-  Object.keys(buttonSettings).forEach((settingKey) => {
-    stringCssSettings += `${buttonSettings[settingKey].cssName} : ${buttonSettings[settingKey].value}${buttonSettings[settingKey].additionalValue};\r`;
+  Object.keys(buttonSettings.style).forEach((settingKey) => {
+    stringCssSettings += `${buttonSettings.style[settingKey].cssName} : ${buttonSettings.style[settingKey].value}${buttonSettings.style[settingKey].additionalValue};\r`;
   });
   windowForDisplayCssSettings.innerText = `
     HTML:
-    <input type="text" value='${buttonBuilder.buttonValue}' id="custom-button-text">
+    <input type="text" value='${buttonSettings.buttonValue}' id="custom-button-text">
     
     CSS: 
     .custom-button-text {
